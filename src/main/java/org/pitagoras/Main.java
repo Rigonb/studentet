575dacfe606dde067a4a7fadd52010019a77fa7c
rigonbashota <bashotarigon6@gmail.com>
2024-09-27 19:06:04 +0200

    new changes

--- src/main/java/org/pitagoras/Main.java
+++ src/main/java/org/pitagoras/Main.java
@@ -24,10 +24,10 @@ public class Main {
         System.out.println(srepo.kthejTeGjitheStudentet());
 
         while (true) {
-            System.out.println("Pres 1 if u want to see the list of the students");
+            System.out.println("Pres 1 if u want to see the list of the students.");
             System.out.println("Pres 2 if u want to update a Student.ª");
-            System.out.println("Pres 3 if u want to delete a Student");
-            System.out.println("Pres x if u want to exit");
+            System.out.println("Pres 3 if u want to delete a Student.");
+            System.out.println("Pres x if u want to exit.");
             String input = scanner.nextLine();
             if (input.equals("x")) {
                 break;
